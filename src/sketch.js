e8d572320a3cb5d14b191b84e84ce0da43403f88
Santi <uo283523@uniovi.es>
2023-10-27 16:36:05 +0200

    Nueva clase Soldier. Al hacer clic se dibuja un soldado en el canvas

--- src/sketch.js
+++ src/sketch.js
@@ -1,16 +1,34 @@
-var mySvg; 
+const pages = "";
 
-function preload(){
+
+
+var mySvg;
+
+function preload() {
+//#ifdef pages 
 	mySvg = loadImage("/img/peace_soldiers.svg");
-
+  flowerThrower = loadImage("/img/flower_thrower.svg");
+//#endif
 }
 
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(1920, 1080);
+  soldiers = [];
 }
 
 function draw() {
   background(255);
   imageMode(CENTER);
-  image(mySvg, 0, 0);
+  image(mySvg, 500, 500);
+  for (var i = 0; i < soldiers.length; i++) {
+    image(flowerThrower, soldiers[i].x, soldiers[i].y, 60, 70); 
+  }
+}
+
+function mouseClicked() {
+  addSoldier(mouseX, mouseY);
+}
+
+function addSoldier(x,y) {
+  soldiers.push(new Soldier(x, y, "red"));
 }
