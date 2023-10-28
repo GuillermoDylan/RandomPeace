4dd61815402616b4fcc7aad801f94976633627ee
Santi <uo283523@uniovi.es>
2023-10-28 03:38:19 +0200

    Los soldados aparecen en un area alrededor del clic

--- src/sketch.js
+++ src/sketch.js
@@ -1,25 +1,21 @@
-const pages = "";
-
-
-
 var mySvg;
 
 function preload() {
-//#ifdef pages 
 	mySvg = loadImage("/img/peace_soldiers.svg");
   flowerThrower = loadImage("/img/flower_thrower.svg");
-//#endif
 }
 
 function setup() {
   createCanvas(1920, 1080);
+  MAX_SOLDIERS = 50;
+  SOLDIER_RANDOM_RANGE = 150;
   soldiers = [];
 }
 
 function draw() {
   background(255);
   imageMode(CENTER);
-  image(mySvg, 500, 500);
+  //image(mySvg, 500, 500);
   for (var i = 0; i < soldiers.length; i++) {
     image(flowerThrower, soldiers[i].x, soldiers[i].y, 60, 70); 
   }
@@ -30,5 +26,14 @@ function mouseClicked() {
 }
 
 function addSoldier(x,y) {
+  if (soldiers.length > MAX_SOLDIERS) {
+    console.log("Max soldiers reached");
+    return;
+  }
+  // circunferencia de radio 50 con centro en x,y
+  x = x + random(-SOLDIER_RANDOM_RANGE, SOLDIER_RANDOM_RANGE);
+  y = y + random(-SOLDIER_RANDOM_RANGE, SOLDIER_RANDOM_RANGE);
+
   soldiers.push(new Soldier(x, y, "red"));
+  console.log("new Soldier added");
 }
