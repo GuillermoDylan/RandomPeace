d7fbabe7e18d32e2b574ba984606a988fa009c3a
Guillermo Dylan Carvajal Aza <98907563+UO283069@users.noreply.github.com>
2023-11-23 12:50:01 +0100

    Avances

--- src/text_generator.js
+++ src/text_generator.js
@@ -20,9 +20,20 @@ class TextGenerator {
     async generateText(){
         var text = await FileReader.read("./res/flowers.txt");
         var flowers = text.split("\n");
-        for(let i = 0; i < this.number; i++){
-            this.text.push(flowers[i]);
-        }
+        var flowerIndex = indices[randomIndex]; // get the corresponding flower index
+        indices.splice(randomIndex, 1); // remove the index from the array
+
+        this.text.push(flowers[flowerIndex]); 
+        /**for(let i = 0; i < this.number; i++){
+            var randomX = Math.floor(Math.random() * (flowers.length));
+            while(this.text.includes(flowers[randomX])){
+                console.log(flowers[randomX] + " is already in the list")
+                console.log(this.text)
+                randomX = Math.floor(Math.random() * (flowers.length));
+                console.log(flowers[randomX] + " is the new random flower")
+            }
+            this.text.push(flowers[randomX]); 
+        }**/
     }
 
 }
